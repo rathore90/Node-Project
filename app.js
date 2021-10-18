71b243a82db24d54a8bd480e25d19260f0a84988
rathore90 <rathore90@hotmail.com>
2021-10-17 21:10:07 -0700

    schema and modal

--- app.js
+++ app.js
@@ -1,54 +1,80 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connection string
+const dbURI = 'mongodb+srv://pardeep_node:44yxbBysXCgZ3bSn@cluster0.n66oq.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
-app.listen(3000, function () {
-  console.log("Express server listening on port 3000");
-});
+// listen for requests
+// app.listen(3000);
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
-
 app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
+// app.get('/add-blog', (req, res) => {
+//   const blog = new Blog({
+//     title: 'new-blog2',
+//     snippet: '2about my new blog',
+//     body: '2more about my new blog'
+//   });
+
+//   blog.save()
+//     .then((result) =>{
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
+
+// app.get('/all-blogs', (req,res)=>{
+//   Blog.find()
+//   .then((result) => {
+//     res.send(result);
+//   }).catch((err)=>{
+//     console.log(err)
+//   })
+// })
+
+// app.get('/single-blog', (req, res) => {
+//   Blog.findById('616ce967bb25d6ad7c49608c')
+//   .then((result) => {
+//     res.send(result)
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+// })
 
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
+
+// blog route
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+  .then((result) => {
+    res.render('index', { title: 'AllBlogs', blogs: result})
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
